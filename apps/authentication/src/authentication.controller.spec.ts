0f001e3b0eefbbc06c4b7b4ec62d1ea6840e524d
Neriya Rosner <neriyarosner@gmail.com>
2023-05-04 16:42:18 +0300

    test as module

--- apps/authentication/src/authentication.controller.spec.ts
+++ apps/authentication/src/authentication.controller.spec.ts
@@ -3,20 +3,18 @@ import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 
 describe('AuthenticationController', () => {
-  let authenticationController: AuthenticationController;
+  let controller: AuthenticationController;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthenticationController],
       providers: [AuthenticationService],
     }).compile();
 
-    authenticationController = app.get<AuthenticationController>(AuthenticationController);
+    controller = module.get<AuthenticationController>(AuthenticationController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(authenticationController.getHello()).toBe('Hello World!');
-    });
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
   });
 });
