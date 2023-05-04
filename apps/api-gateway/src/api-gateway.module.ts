309a52306d1f780cd5ad572489c89f2f0c498a22
Neriya Rosner <neriyarosner@gmail.com>
2023-05-04 20:50:46 +0300

    delete unnecessary files

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ApiGatewayController } from './api-gateway.controller';
-import { ApiGatewayService } from './api-gateway.service';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { PaymentModule } from './payment/payment.module';
 
 @Module({
   imports: [AuthenticationModule, PaymentModule],
-  controllers: [ApiGatewayController],
-  providers: [ApiGatewayService],
 })
 export class ApiGatewayModule {}
