5be9728ba20da5659b75fecd159186fc45f5077e
Neriya Rosner <neriyarosner@gmail.com>
2023-05-04 16:43:00 +0300

    rename

--- packages/common/src/authentication/index.ts
+++ packages/common/src/authentication/index.ts
@@ -1,5 +1,5 @@
 export * from './user.interface';
-export * from './i-authentication-response';
+export * from './authentication-response';
 export * from './create-user.dto';
 export * from './create-user-response.dto';
 export * from './get-user-response.dto';
