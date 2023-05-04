cc21e8ee94c9a8aa6eaf3b7d2124ae9409b151a5
Neriya Rosner <neriyarosner@gmail.com>
2023-05-04 20:52:44 +0300

    connect to mailer microservice

--- apps/payment/src/payment.module.ts
+++ apps/payment/src/payment.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'MAILER',
+        transport: Transport.TCP,
+        options: {
+          host: '127.0.0.1',
+          port: 3003,
+        },
+      },
+    ]),
+  ],
   controllers: [PaymentController],
   providers: [PaymentService],
 })
