a400f51923c79e051b756faf8145f44acc0c9fbf
Neriya Rosner <neriyarosner@gmail.com>
2023-05-04 16:58:29 +0300

    add toJson and remove public password property

--- apps/authentication/src/entities/user.entity.ts
+++ apps/authentication/src/entities/user.entity.ts
@@ -4,11 +4,18 @@ export class User implements IUser {
   private readonly _password: string;
   id: string;
 
-  constructor(public readonly email: string, readonly password: string) {
+  constructor(public readonly email: string, password: string) {
     this._password = password;
   }
 
   isValidPassword(password: string) {
     return this._password === password;
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      email: this.email,
+    };
   }
 }
