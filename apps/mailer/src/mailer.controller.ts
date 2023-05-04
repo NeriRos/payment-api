fe27d18b8f5accbd2ccdf061833e320e189f4bb5
Neriya Rosner <neriyarosner@gmail.com>
2023-05-04 20:52:54 +0300

    create route

--- apps/mailer/src/mailer.controller.ts
+++ apps/mailer/src/mailer.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { MailerService } from './mailer.service';
+import { EventPattern } from '@nestjs/microservices';
+import { SendMailDto } from '@common/mailer';
 
 @Controller()
 export class MailerController {
   constructor(private readonly mailerService: MailerService) {}
 
-  @Get()
-  getHello(): string {
-    return this.mailerService.getHello();
+  @EventPattern('MAILER')
+  sendMail(data: SendMailDto): boolean {
+    return this.mailerService.sendMail(data);
   }
 }
