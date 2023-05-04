4e858ffbab036871898114b9789c52eba02584bc
Neriya Rosner <neriyarosner@gmail.com>
2023-05-04 20:52:15 +0300

    add create payment dto

--- libs/common/src/payment/create-payment.dto.ts
+++ libs/common/src/payment/create-payment.dto.ts
@@ -1,3 +1,5 @@
+import { User } from '@app/authentication/src/entities/user.entity';
+
 export class CreatePaymentDto {
-  constructor(public data: any) {}
+  constructor(public data: any, public user?: User) {}
 }
