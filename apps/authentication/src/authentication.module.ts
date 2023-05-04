2363b84e7644c26b2a1df58026015aa984c1cdb9
Neriya Rosner <neriyarosner@gmail.com>
2023-05-04 16:40:28 +0300

    organize

--- apps/authentication/src/authentication.module.ts
+++ apps/authentication/src/authentication.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { AuthenticationController } from './authentication.controller';
 
 @Module({
-  imports: [],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
 })
