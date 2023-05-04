3cb4dcdca7b2deba27e0870874e7a54cbc60eb41
Neriya Rosner <neriyarosner@gmail.com>
2023-05-04 20:52:30 +0300

    apply microservice

--- apps/mailer/src/main.ts
+++ apps/mailer/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { MailerModule } from './mailer.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MailerModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(MailerModule, {
+    transport: Transport.TCP,
+    options: { port: 3003, host: '127.0.0.1' },
+  });
+  await app.listen();
 }
+
 bootstrap();
